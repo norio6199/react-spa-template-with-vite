b94ac181e26463ad306c3f2f562dd570667f3345
norio6199 <norio6199@gmail.com>
2022-10-11 22:50:11 +0900

    setting eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  root: "./src",
-  publicDir: "../public",
+  base: './',
+  root: './src',
+  publicDir: '../public',
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
     emptyOutDir: true,
   },
   server: {
@@ -15,9 +15,9 @@ export default defineConfig({
   },
   plugins: [
     react({
-      jsxImportSource: "@emotion/react",
+      jsxImportSource: '@emotion/react',
       babel: {
-        plugins: ["@emotion/babel-plugin"],
+        plugins: ['@emotion/babel-plugin'],
       },
     }),
   ],
